b8e59169e228305926197c5ddd4fcd21323b8266
edward <ed_010@mail.ru>
2020-01-13 17:35:53 +0300

    add the ping

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.login("NjY2MjgxNjE2MTIwNzQxOTE5.Xhx7bQ.JBVsDMX953ADYLAuKFWRRO6in3I");
+client.login("NjY2MjgxNjE2MTIwNzQxOTE5.Xhx-eg.-d6kwVCUyOjXUn4BoLtaOU-YEJw");
 
 client.on("message",(message)=>
 {
     if(message.content === "!privet")
     {
         message.reply("Привет!:wave:");
+    }
+    if (message.content === "!ping")
+    {
+        message.channel.send('pong');
     }
 });
